fed630d9debd1d21740f838687ba7c87f974c6b1
Hugo Pereira Da Costa <hugo@oxygen-icons.org>
2011-09-13 18:59:59 +0200

    changed dummy placement location to hopefully force windows to appear on "primary" screen.

--- style/oxygenargbhelper.cpp
+++ style/oxygenargbhelper.cpp
@@ -199,7 +199,7 @@ namespace Oxygen
         somehow the window gets repositioned to <1,<1 and thus always appears in the upper left corner
         we just move it faaaaar away so kwin will take back control and apply smart placement or whatever
         */
-        QPoint position( 10000, 10000 );
+        QPoint position( -10000, 10000 );
 
         // set translucent flag
         widget->setAttribute( Qt::WA_TranslucentBackground );
